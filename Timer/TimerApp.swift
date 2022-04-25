65008f0ead51191d499f97d4c56d62fa7f9626e1
seregious <seregious@gmail.com>
2022-04-25 17:38:58 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Сергей Иванчихин on 25.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
